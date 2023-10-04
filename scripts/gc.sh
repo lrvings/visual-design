805ef5c5ef9cc658327df68c777a083337b56665
liwenju <1018610468@qq.com>
2023-10-04 09:54:53 +0800

    generate components script

---
+++ scripts/gc.sh
@@ -0,0 +1,72 @@
+#! /bin/bash
+
+NAME=$1
+
+FILE_PATH=$(cd "$(dirname "${BASH_SOURCE[0]}")/../packages" && pwd)
+
+re="[[:space:]]+"
+
+if [ "$#" -ne 1 ] || [[ $NAME =~ $re ]] || [ "$NAME" == "" ]; then
+  echo "Usage: pnpm gc \${name} with no space"
+  exit 1
+fi
+
+DIRNAME="$FILE_PATH/components/$NAME"
+INPUT_NAME=$NAME
+
+if [ -d "$DIRNAME" ]; then
+  echo "$NAME component already exists, please change it"
+  exit 1
+fi
+
+NORMALIZED_NAME=""
+for i in $(echo $NAME | sed 's/[_|-]\([a-z]\)/\ \1/;s/^\([a-z]\)/\ \1/'); do
+  C=$(echo "${i:0:1}" | tr "[:lower:]" "[:upper:]")
+  NORMALIZED_NAME="$NORMALIZED_NAME${C}${i:1}"
+done
+NAME=$NORMALIZED_NAME
+
+mkdir -p "$DIRNAME"
+mkdir -p "$DIRNAME/src"
+
+cat > $DIRNAME/src/$INPUT_NAME.vue <<EOF
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ${INPUT_NAME}Props } from './$INPUT_NAME'
+
+defineOptions({
+  name: 'V$NAME',
+})
+
+const props = defineProps(${INPUT_NAME}Props)
+
+// init here
+</script>
+EOF
+
+cat > $DIRNAME/src/$INPUT_NAME.ts <<EOF
+import { buildProps } from '@visual-design/utils'
+
+import type { ExtractPropTypes } from 'vue'
+import type $NAME from './$INPUT_NAME.vue'
+
+export const ${INPUT_NAME}Props = buildProps({})
+
+export type ${NAME}Props = ExtractPropTypes<typeof ${INPUT_NAME}Props>
+export type ${NAME}Instance = InstanceType<typeof $NAME>
+EOF
+
+cat <<EOF >"$DIRNAME/index.ts"
+import { withInstall } from '@visual-design/utils'
+import $NAME from './src/$INPUT_NAME.vue'
+
+export const V$NAME = withInstall($NAME)
+export default V$NAME
+
+export * from './src/$INPUT_NAME'
+EOF
