9e094eeab8607103ed0bed58660c8fd18e9e5cc2
liwenju <1018610468@qq.com>
2023-10-05 14:40:31 +0800

    add types

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -4,6 +4,25 @@ declare global {
       NODE_ENV: string
     }
   }
+  namespace JSX {
+    interface IntrinsicAttributes {
+      class?: any
+      style?: any
+    }
+  }
+}
+declare module '@vue/runtime-core' {
+  export interface App {
+    [INSTALLED_KEY]?: boolean
+  }
+
+  export interface GlobalComponents {
+    Component: (props: { is: Component | string }) => void
+  }
+
+  export interface ComponentCustomProperties {
+    vShow: typeof vShow
+  }
 }
 
 export {}
